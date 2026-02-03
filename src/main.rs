d99dd358ef0c71be19b323499a21f8de2c80612c
zRick156 <robu608@gmail.com>
2026-02-03 22:43:54 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,12 @@ fn main() {
 
         let mut command: String = String::new();
         io::stdin().read_line(&mut command).unwrap();
-        match command.trim() {
+        let command: Vec<&str> = command.split_whitespace().collect();
+        match command[0] {
             "exit" => std::process::exit(0),
-            _ => println!("{}: command not found", command.trim()),
+            "echo" => println!("{}", command[1..].join(" ")),
+            _ => println!("{}: command not found", command[0]),
         }
     }
 }
+
