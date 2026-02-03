48584bbd096fc901c397c4fddd50a91ac0c1fd78
zRick156 <robu608@gmail.com>
2026-02-03 22:26:46 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,14 @@ use std::io::{self, Write};
 fn main() {
     // TODO: Uncomment the code below to pass the first stage
     let mut command: String = String::new();
-    while !command.contains("exit") {
+    loop {
        print!("$ ");
        io::stdout().flush().unwrap();
 
         io::stdin().read_line(&mut command).unwrap();
         if command.contains("exit") {
-            println!("{}: command not found", command.trim());
+            std::process::exit(0);
         }
+        println!("{}: command not found", command.trim());
     }
 }
